289c5669a38f7ff188abfb92549fba5ab8095533
Hasan Tufan YILDIRIM <hasantufanyildirim@gmail.com>
2022-02-06 11:16:54 +0300

    mathhHelper

---
+++ BilgeAdam.Numerator/BilgeAdam.Numerator/HasanTufan/mathhHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilgeAdam.Numerator.HasanTufan
+{
+    class mathhHelper
+    {
+        internal class mathhHelper
+        {
+
+            private  bool IsPrime(int number)
+            {
+                return false;
+            }
+
+        }
+    }
+}
