bd886feacfcc5ac2e9c6f1a5592dd2ff909f9b1a
Harun Metehan Çam <harunmetehan.cam@bilgeadamboost.com>
2022-02-06 11:20:30 +0300

    new RndNumberGuess class

--- BilgeAdam.Numerator/BilgeAdam.Numerator/Metehan/RandomNumberGuess.cs
+++ BilgeAdam.Numerator/BilgeAdam.Numerator/Metehan/RandomNumberGuess.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace BilgeAdam.Numerator.Metehan
 {
-    internal class RandomNumberGuess
+    public static class RandomNumberGuess
     {
-        public static void Random() 
+        public static void RandomGuess() 
         {
             var Guess = int.Parse(Console.ReadLine());
             Console.Write("100'e kadar sayi tahmini: ");
@@ -20,6 +20,14 @@ namespace BilgeAdam.Numerator.Metehan
                 Console.WriteLine("-");
             }
             Console.WriteLine("+");
+        }
+
+        public static void PswCreator()
+        {
+            var passLenght = int.Parse(Console.ReadLine());
+            Console.Write("Sifre Uzunlugu: ");
+            var guid = new Guid();
+            Console.WriteLine($"sifre ({guid.ToString().Remove(0, passLenght)})");
         }
     }
 }
